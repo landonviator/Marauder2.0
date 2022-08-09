5c640d4c59d15cd95c79c31de8211d511f19f64b
Landon <33662627+landonviator@users.noreply.github.com>
2022-08-09 17:32:03 -0500

    Add and position skeu band dials

--- Marauder/Source/GUI/Components/MultibandModule.cpp
+++ Marauder/Source/GUI/Components/MultibandModule.cpp
@@ -13,10 +13,30 @@
 
 //==============================================================================
 MultibandModule::MultibandModule(MarauderAudioProcessor& p, SettingsPage& s) : audioProcessor(p), _settingsPage(s)
+, _band1MixDial(2, " %", 0.0, 100.0)
+, _band2MixDial(2, " %", 0.0, 100.0)
+, _band3MixDial(2, " %", 0.0, 100.0)
+, _band4MixDial(2, " %", 0.0, 100.0)
 {
     addAndMakeVisible(_mainBorder);
     _mainBorder.setText("Multiband Module");
 
+    addAndMakeVisible(_band1MixDial);
+    _band1MixDial.setDoubleClickReturnValue(true, 100.0);
+    
+    addAndMakeVisible(_band2MixDial);
+    _band2MixDial.setDoubleClickReturnValue(true, 100.0);
+    
+    addAndMakeVisible(_band3MixDial);
+    _band3MixDial.setDoubleClickReturnValue(true, 100.0);
+    
+    addAndMakeVisible(_band4MixDial);
+    _band4MixDial.setDoubleClickReturnValue(true, 100.0);
+    
+    addAndMakeVisible(_band1MixDialLabel);
+    addAndMakeVisible(_band2MixDialLabel);
+    addAndMakeVisible(_band3MixDialLabel);
+    addAndMakeVisible(_band4MixDialLabel);
 }
 
 MultibandModule::~MultibandModule()
@@ -30,4 +50,49 @@ void MultibandModule::paint (juce::Graphics& g)
 void MultibandModule::resized()
 {
     _mainBorder.setBounds(getLocalBounds());
+    
+    const auto dialX = getWidth() * 0.01;
+    const auto dialY = getHeight() * 0.12;
+    
+    // Skeuomorphic
+    if (_settingsPage.getUIType())
+    {
+        //activateFlatComps(false);
+        const auto ySkeuDialSpace = 2.0;
+        const auto skeuDialSize = getWidth() * 0.5;
+        const auto toggleX = getWidth() * 0.31;
+        const auto toggleSize = getWidth() * 0.35;
+        
+        _band1MixDial.setBounds(dialX, dialY, skeuDialSize, skeuDialSize);
+        _band2MixDial.setBounds(_band1MixDial.getX() + _band1MixDial.getWidth(), _band1MixDial.getY(), skeuDialSize, skeuDialSize);
+        _band3MixDial.setBounds(dialX, _band2MixDial.getY() + _band2MixDial.getHeight() * ySkeuDialSpace, skeuDialSize, skeuDialSize);
+        _band4MixDial.setBounds(_band1MixDial.getX() + _band1MixDial.getWidth(), _band3MixDial.getY(), skeuDialSize, skeuDialSize);
+        //_skeuPhaseToggle.setBounds(toggleX, _outputDial.getY() + _outputDial.getHeight(), toggleSize, toggleSize);
+        activateSkeuComps(true);
+    }
+}
+
+void MultibandModule::activateSkeuComps(bool shouldBeOn)
+{
+    for (auto& dial : skeuDials)
+    {
+        dial->setEnabled(shouldBeOn);
+        dial->setVisible(shouldBeOn);
+    }
+    
+    // Toggle
+    //_skeuPhaseToggle.setEnabled(shouldBeOn);
+    //_skeuPhaseToggle.setVisible(shouldBeOn);
+    
+    // Switch component attachment
+    if (shouldBeOn)
+    {
+        for (size_t label = 0; label < skeuLabels.size(); ++label)
+        {
+            if (skeuLabels[label]->getAttachedComponent() != skeuDials[label])
+            {
+                skeuLabels[label]->attachToComponent(skeuDials[label], false);
+            }
+        }
+    }
 }
--- Marauder/Source/GUI/Components/MultibandModule.h
+++ Marauder/Source/GUI/Components/MultibandModule.h
@@ -32,5 +32,27 @@ private:
     
     juce::GroupComponent _mainBorder;
     
+    /** Skeuomorphic Comps */
+    viator_gui::FilmStripKnob _band1MixDial;
+    viator_gui::FilmStripKnob _band2MixDial;
+    viator_gui::FilmStripKnob _band3MixDial;
+    viator_gui::FilmStripKnob _band4MixDial;
+    viator_gui::Label _band1MixDialLabel {"Low Band"};
+    viator_gui::Label _band2MixDialLabel {"Low Mid Band"};
+    viator_gui::Label _band3MixDialLabel {"Mid Band"};
+    viator_gui::Label _band4MixDialLabel {"High Band"};
+    
+    void activateSkeuComps(bool shouldBeOn);
+    
+    std::vector<viator_gui::FilmStripKnob*> skeuDials =
+    {
+        &_band1MixDial, &_band2MixDial, &_band3MixDial, &_band4MixDial
+    };
+    
+    std::vector<viator_gui::Label*> skeuLabels =
+    {
+        &_band1MixDialLabel, &_band2MixDialLabel, &_band3MixDialLabel, &_band4MixDialLabel
+    };
+    
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (MultibandModule)
 };
