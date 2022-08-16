4afdb39841317a051f0284ad66a9d62bb3046b53
Landon <33662627+landonviator@users.noreply.github.com>
2022-08-15 21:54:05 -0500

    Add delay skeu and flat dials

--- Marauder/Source/GUI/Components/DelayModule.cpp
+++ Marauder/Source/GUI/Components/DelayModule.cpp
@@ -13,10 +13,40 @@
 
 //==============================================================================
 DelayModule::DelayModule(MarauderAudioProcessor& p, SettingsPage& s) : audioProcessor(p), _settingsPage(s)
+, _delayDial(1, "", 0.0, 1700.0, true)
+, _feedbackDial(1, "", 0.0, 0.95, false)
+, _delayFlatDial("", "", 0.0, 1700.0, 1.0, 0.0)
+, _feedbackFlatDial("", "", 0.0, 95.0, 0.01, 0.0)
 {
     addAndMakeVisible(_mainBorder);
     _mainBorder.setText("Delay Module");
     _mainBorder.setColour(juce::GroupComponent::ColourIds::outlineColourId, juce::Colours::transparentBlack);
+    
+    for (auto& dial : skeuDials)
+    {
+        addAndMakeVisible(dial);
+    }
+    
+    _delayDialAttach = std::make_unique<sliderAttachment>(audioProcessor._treeState, delayTimeID, _delayDial);
+    _feedbackDialAttach = std::make_unique<sliderAttachment>(audioProcessor._treeState, feedbackID, _feedbackDial);
+    
+    for (auto& label : skeuLabels)
+    {
+        addAndMakeVisible(label);
+    }
+    
+    for (auto& dial : flatDials)
+    {
+        addAndMakeVisible(dial);
+    }
+    
+    _delayFlatDialAttach = std::make_unique<sliderAttachment>(audioProcessor._treeState, delayTimeID, _delayFlatDial);
+    _feedbackFlatDialAttach = std::make_unique<sliderAttachment>(audioProcessor._treeState, feedbackID, _feedbackFlatDial);
+    
+    for (auto& label : flatLabels)
+    {
+        addAndMakeVisible(label);
+    }
 }
 
 DelayModule::~DelayModule()
@@ -29,10 +59,112 @@ void DelayModule::paint (juce::Graphics& g)
     g.setColour(m_mainCompColor.withAlpha(0.2f));
     g.drawLine(0, getHeight() * 0.125, 0, getHeight() * 0.875, 3);
     
+    for (auto& dial : skeuDials)
+    {
+        dial->updateLabelColor(m_textAccentColor.withLightness(0.75f));
+    }
+    
+    for (auto& label : skeuLabels)
+    {
+        label->setColour(juce::Label::ColourIds::textColourId, m_textAccentColor.withLightness(0.75f));
+    }
+    
+    for (auto& dial : flatDials)
+    {
+        updateSliderColors(*dial);
+    }
+    
     _mainBorder.setColour(juce::GroupComponent::ColourIds::textColourId, m_textAccentColor.withLightness(0.75f).withAlpha(0.5f));
 }
 
 void DelayModule::resized()
 {
     _mainBorder.setBounds(getLocalBounds().withY(getHeight() * 0.015));
+    
+    const auto dialX = getWidth() * 0.088;
+    const auto dialY = getHeight() * 0.29;
+    const auto ySkeuDialSpace = 1.35;
+    const auto xSkeuDialSpace = 1.2;
+    const auto skeuDialSize = getWidth() * 0.38;
+    const auto flatDialSize = getWidth() * 0.4;
+    const auto yFlatDialSpace = 1.25;
+    
+    if (_settingsPage.getUIType())
+    {
+        _delayDial.setBounds(dialX, dialY, skeuDialSize, skeuDialSize);
+        _feedbackDial.setBounds(_delayDial.getX() + _delayDial.getWidth() * xSkeuDialSpace, dialY, skeuDialSize, skeuDialSize);
+        activateFlatComps(false);
+        activateSkeuComps(true);
+    }
+    
+    else
+    {
+        _delayFlatDial.setBounds(dialX, dialY, skeuDialSize, skeuDialSize);
+        _feedbackFlatDial.setBounds(_delayFlatDial.getX() + _delayFlatDial.getWidth() * xSkeuDialSpace, dialY, skeuDialSize, skeuDialSize);
+        activateFlatComps(true);
+        activateSkeuComps(false);
+    }
+}
+
+void DelayModule::activateSkeuComps(bool shouldBeOn)
+{
+    for (auto& dial : skeuDials)
+    {
+        dial->setEnabled(shouldBeOn);
+        dial->setVisible(shouldBeOn);
+    }
+    
+    if (shouldBeOn)
+    {
+        for (size_t label = 0; label < skeuLabels.size(); ++label)
+        {
+            if (skeuLabels[label]->getAttachedComponent() != skeuDials[label])
+            {
+                skeuLabels[label]->attachToComponent(skeuDials[label], false);
+            }
+        }
+    }
+}
+
+void DelayModule::activateFlatComps(bool shouldBeOn)
+{
+    for (auto& dial : flatDials)
+    {
+        dial->setEnabled(shouldBeOn);
+        dial->setVisible(shouldBeOn);
+    }
+    
+    if (shouldBeOn)
+    {
+        for (size_t label = 0; label < skeuLabels.size(); ++label)
+        {
+            if (skeuLabels[label]->getAttachedComponent() != flatDials[label])
+            {
+                skeuLabels[label]->attachToComponent(flatDials[label], false);
+            }
+        }
+    }
+}
+
+void DelayModule::updateSliderColors(viator_gui::Dial &slider)
+{
+    slider.setColour(juce::Slider::ColourIds::rotarySliderFillColourId, m_mainCompFillColor);
+    slider.setColour(juce::Slider::ColourIds::rotarySliderOutlineColourId, m_bgLighterColor);
+    slider.setColour(juce::Slider::ColourIds::trackColourId, m_textAccentColor);
+    slider.setColour(juce::Slider::ColourIds::textBoxOutlineColourId, juce::Colours::black.withAlpha(0.0f));
+    slider.setColour(juce::Slider::ColourIds::textBoxTextColourId, m_textAccentColor);
+    slider.setColour(juce::Slider::ColourIds::thumbColourId, m_mainCompColor);
+    slider.setColour(juce::Slider::ColourIds::backgroundColourId, m_bgTransparent);
+    
+    if (_settingsPage.getGlowState())
+    {
+        slider.updateShadowColor(m_mainCompColor, _settingsPage.getCurrentGlowValue());
+    }
+    
+    else
+    {
+        slider.updateShadowColor(juce::Colours::black, 0.4f);
+    }
+    
+    slider.forceShadow();
 }
--- Marauder/Source/GUI/Components/DelayModule.h
+++ Marauder/Source/GUI/Components/DelayModule.h
@@ -31,5 +31,48 @@ private:
     SettingsPage& _settingsPage;
     
     juce::GroupComponent _mainBorder;
+    
+    using sliderAttachment = juce::AudioProcessorValueTreeState::SliderAttachment;
+    
+    /** Skeuomorphic Comps */
+    viator_gui::FilmStripKnob _delayDial;
+    viator_gui::FilmStripKnob _feedbackDial;
+    viator_gui::Label _delayDialLabel {"Tension"};
+    viator_gui::Label _feedbackDialLabel {"Intensity"};
+    std::unique_ptr<sliderAttachment> _delayDialAttach;
+    std::unique_ptr<sliderAttachment> _feedbackDialAttach;
+    
+    std::vector<viator_gui::FilmStripKnob*> skeuDials =
+    {
+        &_delayDial, &_feedbackDial
+    };
+    
+    std::vector<viator_gui::Label*> skeuLabels =
+    {
+        &_delayDialLabel, &_feedbackDialLabel
+    };
+    
+    void activateSkeuComps(bool shouldBeOn);
+    
+    /** Flat Comps */
+    viator_gui::Dial _delayFlatDial;
+    viator_gui::Dial _feedbackFlatDial;
+    viator_gui::Label _delayFlatDialLabel {"Tension"};
+    viator_gui::Label _feedbackFlatDialLabel {"Intensity"};
+    std::unique_ptr<sliderAttachment> _delayFlatDialAttach;
+    std::unique_ptr<sliderAttachment> _feedbackFlatDialAttach;
+    
+    std::vector<viator_gui::Dial*> flatDials =
+    {
+        &_delayFlatDial, &_feedbackFlatDial
+    };
+    
+    std::vector<viator_gui::Label*> flatLabels =
+    {
+        &_delayFlatDialLabel, &_feedbackFlatDialLabel
+    };
+    
+    void activateFlatComps(bool shouldBeOn);
+    void updateSliderColors(viator_gui::Dial& slider);
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (DelayModule)
 };
